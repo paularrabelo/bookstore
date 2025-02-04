02c3bccee883d3c159cbe78244a310076c291af8
Paula Rabelo <paula_rabelo2010@hotmail.com>
2025-02-04 17:09:41 -0300

    Paginação

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -131,3 +131,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
