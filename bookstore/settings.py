79b40bc9f6f5b0378e5b6e5d21e772ca041bf964
Paula Rabelo <paula_rabelo2010@hotmail.com>
2025-02-13 11:22:03 -0300

    Deploy no Dokku

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -144,5 +144,5 @@ REST_FRAMEWORK = {
     ],
 }
 
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api.dokku.me']
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api.dokku.com']
