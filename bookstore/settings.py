f2ddd8becf6461811d0087f43cba1eb8e1ea6a1a
Paula Rabelo <paula_rabelo2010@hotmail.com>
2025-02-17 12:34:58 -0300

    RenderDeploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,4 +148,4 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api-ucfn.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api-ucfn.onrender.com']
